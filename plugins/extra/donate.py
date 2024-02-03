3dc1237ff4b44054b941450ff3a2af1b667538c5
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2024-02-03 09:56:09 +0530

    Add files via upload

---
+++ plugins/extra/donate.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters
+
+from Bikash import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(
+    filters.command("donate")
+    & filters.group)
+async def donate(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
+        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐃𝐨𝐧𝐚𝐭𝐞 & 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐇𝐚𝐲𝐚𝐭𝐨 𝐅𝐨𝐫 𝐐𝐫 𝐂𝐨𝐝𝐞, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](@kinghyt_chat) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 [𝐜𝐡𝐚𝐭](@kinghyt_chat)..""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐇𝐚𝐲𝐚𝐭𝐨 🥀", url=f"https://t.me/K_King_HYT")
+            ],          
+            [
+                    InlineKeyboardButton(
+                        "🥀 𝐇𝐚𝐲𝐚𝐭𝐨 🥀", url=f"https://t.me/K_King_HYT")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/kinghyt_chat"
+                    ),
+                    InlineKeyboardButton(
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/kinghyt_chat")
+                ]
+            ]
+        ),
+    )
